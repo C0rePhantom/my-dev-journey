bb6222c363124364658f28bd09c91b3a419009ad
Timur <saadsi.git@gmail.com>
2026-03-24 03:09:04 +0300

    feat: improve CLI ToDo (commands, help, normalization)

--- cpp/cli-todo/main.cpp
+++ cpp/cli-todo/main.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-	std::string command;
+    std::vector<std::string> tasks;
+    std::string command;
+    std::string task;
 
-	std::cout << "CLI ToDo started\n";
+    std::cout << "CLI ToDo started\n";
+    std::cout << "Type 'help' for commands\n";
 
-	while (true)
-	{
-		std::cout << "> ";
-		std::getline(std::cin, command);
+    while (true)
+    {
+        std::cout << "> ";
+        std::getline(std::cin, command);
 
-		if (command == "exit") break;
-		else if (command == "hello")
-			std::cout << "Hello, dev!\n";
-		else
-			std::cout << "Unkown command!\n";
-	}
-	return 0;
+        // привести команду к нижнему регистру
+        std::transform(command.begin(), command.end(), command.begin(),
+            [](unsigned char c) { return std::tolower(c); });
+
+        if (command == "add")
+        {
+            std::cout << "Enter task: ";
+            std::getline(std::cin >> std::ws, task);
+            tasks.push_back(task);
+            std::cout << "Task added!\n";
+        }
+        else if (command == "list")
+        {
+            if (tasks.empty())
+            {
+                std::cout << "No tasks yet\n";
+                continue;
+            }
+
+            for (size_t i = 0; i < tasks.size(); i++)
+            {
+                std::cout << i + 1 << ". " << tasks[i] << std::endl;
+            }
+        }
+        else if (command == "help")
+        {
+            std::cout << "Commands:\n";
+            std::cout << "add  - add new task\n";
+            std::cout << "list - show all tasks\n";
+            std::cout << "exit - quit program\n";
+        }
+        else if (command == "exit")
+        {
+            std::cout << "Bye!\n";
+            break;
+        }
+        else
+        {
+            std::cout << "Unknown command. Type 'help'\n";
+        }
+    }
+
+    return 0;
 }
